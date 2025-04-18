b27a38b60edb17feadf8f6150d2e9081ec770631
Daniel Li <danli@lcs.on.ca>
2025-04-18 14:35:38 -0400

    Adding model of laundry item with 2 examples

--- Rashleigh Laundry Scheduler/Model/LaundryItem.swift
+++ Rashleigh Laundry Scheduler/Model/LaundryItem.swift
@@ -5,3 +5,26 @@
 //  Created by 李泽宇 on 2025-04-18.
 //
 
+import Foundation
+
+struct Booking: Identifiable {
+    let id = UUID()
+    let date: String
+    let userID: String
+    let startTime: String
+    let endTime: String
+}
+
+let exampleBook1 = Booking(
+    date: "Sunday",
+    userID: "lucuszhu@lcs.on.ca",
+    startTime: "13:00",
+    endTime: "15:00"
+)
+
+let exampleBook2 = Booking(
+    date: "Monday",
+    userID: "DavidShan@lcs.on.ca",
+    startTime: "18:00",
+    endTime: "20:00"
+)
--- Rashleigh Laundry Scheduler/ViewModel/LaundryViewModel.swift
+++ Rashleigh Laundry Scheduler/ViewModel/LaundryViewModel.swift
@@ -4,21 +4,3 @@
 //
 //  Created by 李泽宇 on 2025-04-18.
 //
-
-import SwiftUI
-
-struct LaundryViewModel: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
-
-#Preview {
-    LaundryViewModel()
-}
