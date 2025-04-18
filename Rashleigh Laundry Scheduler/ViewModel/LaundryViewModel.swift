513f4f29b787b5f693bbadbf2519318e817e7516
Daniel Li <danli@lcs.on.ca>
2025-04-18 14:44:38 -0400

    add ViewModel and the function of addBooking with newBooking

--- Rashleigh Laundry Scheduler/ViewModel/LaundryViewModel.swift
+++ Rashleigh Laundry Scheduler/ViewModel/LaundryViewModel.swift
@@ -4,3 +4,25 @@
 //
 //  Created by 李泽宇 on 2025-04-18.
 //
+
+
+import Foundation
+
+@Observable
+
+class LaundryViewModel {
+    var bookings: [Booking] = [
+        exampleBook1,
+        exampleBook2
+    ]
+    
+    func addBooking(date: String, user: String, start: String, end: String) {
+        let newBooking = Booking(
+                date: date,
+                userID: user,
+                startTime: start,
+                endTime: end
+        )
+        bookings.append(newBooking)
+    }
+}
