84e3277f9971e97cefe7aaec99c0c112796c179e
Daniel Li <danli@lcs.on.ca>
2025-04-18 13:38:22 -0400

    Initial Commit

---
+++ Rashleigh Laundry Scheduler/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rashleigh Laundry Scheduler
+//
+//  Created by 李泽宇 on 2025-04-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rashleigh Laundry Scheduler/Rashleigh_Laundry_SchedulerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rashleigh_Laundry_SchedulerApp.swift
+//  Rashleigh Laundry Scheduler
+//
+//  Created by 李泽宇 on 2025-04-18.
+//
+
+import SwiftUI
+
+@main
+struct Rashleigh_Laundry_SchedulerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
