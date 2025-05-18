88da9d3f7d0ce9681515963f12099cc8dd959f51
Daniel Li <danli@lcs.on.ca>
2025-05-18 16:01:34 -0400

    adding another new view of laundry tip view to create the view of fetching laundry tip

---
+++ Rashleigh Laundry Scheduler/Views/LaundryTipView.swift
@@ -0,0 +1,41 @@
+//
+//  LaundryTipView.swift
+//  Rashleigh Laundry Scheduler
+//
+//  Created by 李泽宇 on 2025-05-18.
+//
+
+import SwiftUI
+
+struct LaundryTipView: View {
+    @EnvironmentObject var viewModel: LaundryViewModel
+
+    var body: some View {
+        VStack {
+            Text("🧺 Laundry Tip for today")
+                .font(.title2)
+                .padding(.vertical)
+
+            if let tip = viewModel.currentTip {
+                Text(tip.slip.advice)
+                    .multilineTextAlignment(.center)
+                    .padding()
+            } else {
+                Text("Loading tip...💭")
+                    .foregroundColor(.gray)
+            }
+        }
+        .padding()
+        .background(Color.green.opacity(0.1))
+        .cornerRadius(16)
+        // function of fetching the laundry tip
+        .task {
+            await viewModel.fetchLaundryTip()
+        }
+    }
+}
+
+#Preview{
+    LaundryTipView()
+        .environmentObject(LaundryViewModel())
+}
